6dac03df9306fccb21c4aebd2c7ba12faaf9a99e
watorvapor <watorvapor@gmail.com>
2022-09-04 08:52:16 +0000

    gateway

--- gateway@maap/x.run.sh
+++ gateway@maap/x.run.sh
@@ -4,14 +4,15 @@ PARENT_DiR=$(dirname $SCRIPT_DIR)
 echo "SCRIPT_DIR:=${SCRIPT_DIR}"
 echo "PARENT_DiR:=${PARENT_DiR}"
 docker stop ${DOCKER_MAME}
-docker rm ${DOCKER_MAME}
 docker run -it \
   -v /etc/group:/etc/group:ro \
   -v /etc/passwd:/etc/passwd:ro \
   -v /dev/shm/:/dev/shm/ \
   -v ${PARENT_DiR}:${PARENT_DiR} \
+  -v ${SCRIPT_DIR}:${SCRIPT_DIR} \
+  -v ${HOME}:${HOME} \
   -u $(id -u $USER):$(id -g $USER) \
-  -w ${PARENT_DiR} \
+  -w ${SCRIPT_DIR} \
   --net host \
   --memory=256M \
   --cpu-shares=128 \
