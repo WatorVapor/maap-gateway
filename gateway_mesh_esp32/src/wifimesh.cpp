a45f9a0fae1904306e7871ce1648a8a0564470f8
Wator Vapor <watorvapor@gmail.com>
2022-12-23 23:43:10 +0900

    mesh buffer size.

--- gateway_mesh_esp32/src/wifimesh.cpp
+++ gateway_mesh_esp32/src/wifimesh.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
-
+#include <ArduinoJson.h>
+#include <mutex>
 #include "painlessMesh.h"
 #define   MESH_PREFIX     "pYZviUVDQrjaNsD5"
 #define   MESH_PASSWORD   "Fqji4aPp"
@@ -8,29 +9,73 @@
 static Scheduler userScheduler;
 static painlessMesh  mesh;
 static void sendMessage(void);
-static Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );
+static Task taskSendMessage( TASK_MILLISECOND * 10 , TASK_FOREVER, &sendMessage );
+static String serial2MeshBuff;
+static std::mutex serial2MeshBuffMtx;
+static const size_t iConstBufferMax = 512;
+static StaticJsonDocument<512> mesh2SerialDoc;
+static const size_t iConstMesh2SerialBufferMax = 512 -64;
 
 void sendMessage(void) {
-  String msg = "Hello from node ";
-  msg += mesh.getNodeId();
+  String msg;
+  {
+    std::lock_guard<std::mutex> lock(serial2MeshBuffMtx);
+    if(serial2MeshBuff.isEmpty()) {
+      return;
+    } else {
+      msg = serial2MeshBuff;
+      serial2MeshBuff.clear();
+    }
+  }
   mesh.sendBroadcast( msg );
-  taskSendMessage.setInterval( random( TASK_SECOND * 1, TASK_SECOND * 5 ));
+  taskSendMessage.setInterval( random( TASK_MILLISECOND * 1, TASK_MILLISECOND * 5 ));
 }
 
 void receivedCallback( uint32_t from, String &msg ) {
-  Serial.printf("startHere: Received from %u msg=%s\r\n", from, msg.c_str());
+  mesh2SerialDoc.clear();
+  mesh2SerialDoc["from"] = from;
+  if(msg.length() < iConstMesh2SerialBufferMax) {
+    mesh2SerialDoc["recv"] = msg;
+  } else {
+    String shortedMsg(msg.c_str(),iConstMesh2SerialBufferMax);
+    shortedMsg += "...";
+    mesh2SerialDoc["recv"] = shortedMsg;
+    mesh2SerialDoc["error"] = "over";
+  }
+  std::string outStr;
+  serializeJson(mesh2SerialDoc, outStr);
+  outStr += "\r\n";
+  Serial.print(outStr.c_str());
 }
 
 void newConnectionCallback(uint32_t nodeId) {
-  Serial.printf("--> startHere: New Connection, nodeId = %u\r\n", nodeId);
+  mesh2SerialDoc.clear();
+  mesh2SerialDoc["connect"] = "new";
+  mesh2SerialDoc["nodeId"] = nodeId;
+  std::string outStr;
+  serializeJson(mesh2SerialDoc, outStr);
+  outStr += "\r\n";
+  Serial.print(outStr.c_str());
 }
 
 void changedConnectionCallback() {
-  Serial.printf("Changed connections\r\n");
+  mesh2SerialDoc.clear();
+  mesh2SerialDoc["connect"] = "changed";
+  std::string outStr;
+  serializeJson(mesh2SerialDoc, outStr);
+  outStr += "\r\n";
+  Serial.print(outStr.c_str());
 }
 
 void nodeTimeAdjustedCallback(int32_t offset) {
-  Serial.printf("Adjusted time %u. Offset = %d\r\n", mesh.getNodeTime(),offset);
+  mesh2SerialDoc.clear();
+  mesh2SerialDoc["connect"] = "Adjusted time";
+  mesh2SerialDoc["nodeTime"] = mesh.getNodeTime();
+  mesh2SerialDoc["offset"] = offset;
+  std::string outStr;
+  serializeJson(mesh2SerialDoc, outStr);
+  outStr += "\r\n";
+  Serial.print(outStr.c_str());
 }
 
 void setup_wifi_mesh(void) {
@@ -48,6 +93,17 @@ void WifiMeshTask( void * parameter) {
   setup_wifi_mesh();
   while(true) {
     mesh.update();
-    delay(100);
+    if (Serial.available()) {
+      int inByte = Serial.read();
+      String strByte((char)inByte);
+      {
+        std::lock_guard<std::mutex> lock(serial2MeshBuffMtx);
+        serial2MeshBuff += strByte;
+        if(serial2MeshBuff.length() > iConstBufferMax) {
+          serial2MeshBuff.clear();
+        }
+      }      
+    }
+    delay(10);
   }
 }
