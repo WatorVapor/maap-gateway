2ba29723b5e71943018c1094ff39de85e75b01fa
Wator Vapor <watorvapor@gmail.com>
2022-12-29 22:53:25 +0900

    mesh setting

--- gateway_mesh_esp32/src/main.cpp
+++ gateway_mesh_esp32/src/main.cpp
@@ -1,14 +1,19 @@
 #include <Arduino.h>
 #include <FS.h>
 #include <SPIFFS.h>
+#include "debug.hpp"
+
 void WifiMeshTask( void * parameter);
+void BLESettingTask( void * parameter);
 void setup(void) {
   Serial.begin(115200);
   Serial.println(__DATE__);
   Serial.println(__TIME__);
-  xTaskCreatePinnedToCore(WifiMeshTask, "WifiMeshTask", 1024*100, nullptr, 1, nullptr,  0); 
+  xTaskCreatePinnedToCore(WifiMeshTask, "WifiMeshTask", 1024*10, nullptr, 1, nullptr,  0); 
+  xTaskCreatePinnedToCore(BLESettingTask, "BLESettingTask", 1024*10, nullptr, 1, nullptr,  0); 
 }
 
 void loop(void) {
 }
 
+std::mutex print_mtx_;
