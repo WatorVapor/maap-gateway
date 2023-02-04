b6a8149ccd3aa8d467940e785442d714175a0d7e
Wator Vapor <watovapor@gmail.com>
2023-02-04 04:03:37 +0000

    maap

--- gateway-wifi-mesh/x.run.sh
+++ gateway-wifi-mesh/x.run.sh
@@ -3,6 +3,7 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 PARENT_DiR=$(dirname $SCRIPT_DIR)
 echo "SCRIPT_DIR:=${SCRIPT_DIR}"
 echo "PARENT_DiR:=${PARENT_DiR}"
+DOCKER_MAME="maap-gate-wifi-mesh"
 docker stop ${DOCKER_MAME}
 docker run -it \
   -v /etc/group:/etc/group:ro \
@@ -14,6 +15,7 @@ docker run -it \
   -u $(id -u $USER):$(id -g $USER) \
   -w ${SCRIPT_DIR} \
   --net host \
+  --name ${DOCKER_MAME} \
   --memory=256M \
   --cpu-shares=128 \
   node:lts /bin/bash
