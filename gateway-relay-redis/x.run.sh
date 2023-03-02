ca4e61af024b24aace635b00196dc6724c79b888
Wator Vapor <watovapor@gmail.com>
2023-03-02 12:08:50 +0000

    maap

---
+++ gateway-relay-redis/x.run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+PARENT_DiR=$(dirname $SCRIPT_DIR)
+echo "SCRIPT_DIR:=${SCRIPT_DIR}"
+echo "PARENT_DiR:=${PARENT_DiR}"
+docker run -it \
+  -v /etc/group:/etc/group:ro \
+  -v /etc/passwd:/etc/passwd:ro \
+  -v /dev/shm/:/dev/shm/ \
+  -v ${PARENT_DiR}:${PARENT_DiR} \
+  -v ${SCRIPT_DIR}:${SCRIPT_DIR} \
+  -v ${HOME}:${HOME} \
+  -u $(id -u $USER):$(id -g $USER) \
+  -w ${SCRIPT_DIR} \
+  --net host \
+  --memory=256M \
+  --cpus="1.5" \
+  redis:7 /bin/bash
